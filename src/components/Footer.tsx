509304e4f7d16d176324cde5637d41e8ca94ce9a
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2023-09-16 02:52:27 +0900

    Modify top page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,6 @@
+export const Footer = () =>
+  <footer className="footer has-background-white">
+    <div className="content has-text-centered">
+      <p>All rights reserved © murajun1978 2023</p>
+    </div>
+  </footer>
