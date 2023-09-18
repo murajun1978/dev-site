d49d7074cf4174be55a706ed8a0778e3a2f15d0e
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2023-09-18 22:01:13 +0900

    Modify top page style using a Tailwind CSS

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () =>
-  <footer className="footer has-background-white">
-    <div className="content has-text-centered">
-      <p>All rights reserved © murajun1978 2023</p>
+  <footer>
+    <div className="text-center mb-1">
+      <p className="dark:text-white">All rights reserved © murajun1978 2023</p>
     </div>
   </footer>
